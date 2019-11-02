98e0225ce5d9fb98bd52a33b6b70a311e33bbeef
bharatkram <bharat.bharatkram@msitprogram.net>
2019-11-02 10:32:50 +0530

    final

--- Day- 4/Assignment - 1/Solution.java
+++ Day- 4/Assignment - 1/Solution.java
@@ -1,7 +1,69 @@
-class Solution{
-	public static int[] quickSort(int[] arr){
-		// fill you code Here
+final class Solution {
+	private Solution() {
+	}
+
+	/**
+	 * function to call pass the array to the sub functions.
+	 * boxes and unboxes the array.
+	 *
+	 * @param arr the array that is being passed.
+	 * @return the sorted array.
+	 */
+	public static int[] quickSort(final int[] arr) {
+		sort(arr, 0, arr.length - 1);
 		return arr;
 	}
-	
+
+	/**
+	 * function to sort the array in ascending order recursively.
+	 *
+	 * @param arr the array to be sorted.
+	 * @param lo the starting position of the array.
+	 * @param hi the ending position of the array.
+	 */
+	private static void sort(final int[] arr, final int lo, final int hi) {
+		if (hi <= lo) {
+			return;
+		}
+		int j = partition(arr, lo, hi);
+		sort(arr, lo, j - 1);
+		sort(arr, j + 1, hi);
+	}
+
+	/**
+	 * function to divide the function based on the pivot position.
+	 *
+	 * @param arr the array to be sorted.
+	 * @param lo the position of the starting element.
+	 * @param hi the position of the last element.
+	 * @return the
+	 */
+	private static int partition(final int[] arr, final int lo,
+				final int hi) {
+		int i = lo;
+		int j = hi + 1;
+		while (true) {
+			while (arr[++i] < arr[lo]) {
+				if (i == hi) {
+					break;
+				}
+			}
+			while (arr[lo] < arr[--j]) {
+				if (j == lo) {
+					break;
+				}
+			}
+			if (i >= j) {
+				break;
+			}
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+
+		int temp = arr[lo];
+		arr[lo] = arr[j];
+		arr[j] = temp;
+		return j;
+	}
 }
--- Day- 4/Assignment - 2/Solution.java
+++ Day- 4/Assignment - 2/Solution.java
@@ -1,7 +1,64 @@
-class Solution{
-	public static String[] mergeSort(String[] arr){
-		// fill you code Here
+final class Solution {
+	private Solution() {
+	}
+
+	/**
+	 * function to call the sort function.
+	 *
+	 * @param arr the array to be sorted.
+	 * @return the sorted array.
+	 */
+	public static String[] mergeSort(final String[] arr) {
+		String[] aux = new String[arr.length];
+		sort(arr, aux, 0, arr.length - 1);
 		return arr;
 	}
-	
+
+	/**
+	 * the function to sort the array in ascending order.
+	 *
+	 * @param arr the array to be sorted.
+	 * @param aux auxiliary array for temporary storage of elements.
+	 * @param lo the starting position of the array.
+	 * @param hi the ending position of the array.
+	 */
+	private static void sort(final String[] arr, final String[] aux,
+				final int lo, final int hi) {
+		if (hi <= lo) {
+			return;
+		}
+		int mid = (lo + hi) / 2;
+		sort(arr, aux, lo, mid);
+		sort(arr, aux, mid + 1, hi);
+		merge(arr, aux, lo, mid, hi);
+	}
+
+	/**
+	 * the function to merge the parts in ascending order.
+	 *
+	 * @param arr the array to be sorted.
+	 * @param aux temporary array to store elements.
+	 * @param lo the starting position of the array.
+	 * @param mid the mid position of the array.
+	 * @param hi the last position of the array.
+	 */
+	private static void merge(final String[] arr, final String[] aux,
+				final int lo, final int mid, final int hi) {
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = arr[k];
+		}
+		int i = lo;
+		int j = mid + 1;
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid) {
+				arr[k] = aux[j++];
+			} else if (j > hi) {
+				arr[k] = aux[i++];
+			} else if (aux[j].compareTo(aux[i]) < 0) {
+				arr[k] = aux[j++];
+			} else {
+				arr[k] = aux[i++];
+			}
+		}
+	}
 }
