42519b845bf6bec1e45dfc52c29c1ed18cd0853f
bharatkram <bharat.bharatkram@msitprogram.net>
2019-11-05 16:30:08 +0530

    final

--- Day- 6/Assignment - 2/Solution.java
+++ Day- 6/Assignment - 2/Solution.java
@@ -1,46 +1,58 @@
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-class Solution {
-	public static double[] dynamicMedian(double[] arr) {
-		PriorityQueue<Double> minQ = new PriorityQueue<Double>();
-		PriorityQueue<Double> maxQ = new PriorityQueue<Double>(Collections.reverseOrder());
+final class Solution {
 
-		double[] res = new double[arr.length];
-		double median = arr[0];
+    private Solution() {
+    }
 
-		minQ.add(median);
-		res[0] = median;
+    /**
+     * function to find the dynamic median of the given array.
+     * @param arr the input array containing the elements.
+     * @return the medians found per insertion of array.
+     */
+    public static double[] dynamicMedian(final double[] arr) {
 
-		for (int i = 1; i < arr.length; i++) {
-			if (minQ.size() > maxQ.size()) {
-				if (arr[i] > median) {
-					maxQ.add(minQ.remove());
-					minQ.add(arr[i]);
-				} else {
-					maxQ.add(arr[i]);
-				}
-				median = (minQ.peek() + maxQ.peek()) / 2.0;
-			} else if (minQ.size() < maxQ.size()) {
-				if (arr[i] < median) {
-					minQ.add(maxQ.remove());
-					maxQ.add(arr[i]);
-				} else {
-					minQ.add(arr[i]);
-				}
-				median = (minQ.peek() + maxQ.peek()) / 2.0;
-			} else {
-				if (arr[i] < median) {
-					maxQ.add(arr[i]);
-					median = maxQ.peek();
-				} else {
-					minQ.add(arr[i]);
-					median = minQ.peek();
-				}
-			}
-			res[i] = median;
-		}
+        PriorityQueue<Double> minQ = new PriorityQueue<Double>();
 
-		return res;
-	}
+        PriorityQueue<Double> maxQ = new PriorityQueue<Double>(
+                                        Collections.reverseOrder());
+
+        double[] res = new double[arr.length];
+
+        double median = arr[0];
+
+        minQ.add(median);
+        res[0] = median;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (minQ.size() > maxQ.size()) {
+                if (arr[i] > median) {
+                    maxQ.add(minQ.remove());
+                    minQ.add(arr[i]);
+                } else {
+                    maxQ.add(arr[i]);
+                }
+                median = (minQ.peek() + maxQ.peek()) / 2.0;
+            } else if (minQ.size() < maxQ.size()) {
+                if (arr[i] < median) {
+                    minQ.add(maxQ.remove());
+                    maxQ.add(arr[i]);
+                } else {
+                    minQ.add(arr[i]);
+                }
+                median = (minQ.peek() + maxQ.peek()) / 2.0;
+            } else {
+                if (arr[i] < median) {
+                    maxQ.add(arr[i]);
+                    median = maxQ.peek();
+                } else {
+                    minQ.add(arr[i]);
+                    median = minQ.peek();
+                }
+            }
+            res[i] = median;
+        }
+        return res;
+    }
 }
